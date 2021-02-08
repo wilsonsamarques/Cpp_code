e35484c1e46a93c0cda77ee166cb5af0d6fe3be5
wilsonsamarques <wilsonsamarques@gmail.com>
2021-02-08 17:49:10 -0300

    Lesson 2

---
+++ ProgramStructure/main.cpp
@@ -0,0 +1,12 @@
+// Program to print strings on screen
+/*main program*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world! \n";
+    cout << "Hello world!"    << endl;
+    return 0;
+}
