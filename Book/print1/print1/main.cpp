30b2ba47690038e1bd310933d142f00f04285670
wilsonsamarques <wilsonsamarques@gmail.com>
2021-05-22 12:04:24 -0300

    code written in the past few weeks

---
+++ Book/print1/print1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "Never fear, C++ is here! ";
+	return 0;
+}
--- Snake_game/Snake_game/main.cpp
+++ Snake_game/Snake_game/main.cpp
@@ -7,8 +7,13 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
+
+
+
 
 void Setup()
 {
@@ -39,7 +44,19 @@ void Draw()
 			else if (i == fruitY && j == fruitX)
 				cout << "F";
 			else
-				cout << " ";
+			{
+				bool print = false;
+				for (int k = 0; k < nTail; k++)
+				{
+					if (tailX[k] == j && tailY[k] == i)
+					{
+						cout << "o";
+						print = true;
+					}
+				}
+				if (!print)
+					cout << " ";
+			}
 			if (j == width - 1)
 				cout << "#";
 		}
@@ -77,6 +94,21 @@ void Input()
 }
 void Logic()
 {
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+	for (int i = 1; i < nTail; i++)
+	{
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+	}
+
 	switch (dir)
 	{
 	case LEFT:
@@ -96,11 +128,15 @@ void Logic()
 	}
 	if (x > width || x < 0 || y > height || y < 0)
 		gameOver = true;
+	for (int i = 0; i < nTail; i++)
+		if (tailX[i] == x && tailY[i] == y)
+			gameOver = true;
 	if (x == fruitX && y == fruitY)
 	{
 		score += 10;
 		fruitX = rand() % width;
 		fruitY = rand() % height;
+		nTail++;
 	}
 	
 
@@ -114,7 +150,7 @@ int main()
 		Draw();
 		Input();
 		Logic();
-		Sleep(10); 
+		Sleep(50); 
 
 	}
 
